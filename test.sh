101a180d93a4e06a51b6bf158b221992c0d00df3
Son Geon <fjen6b@gmail.com>
2019-06-23 20:18:03 +0900

    add test.sh to test the senario

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#run three nodes
+./node -ip=127.0.0.1 -port=10000 
+./node -ip=127.0.0.1 -port=10001 -successor-ip=127.0.0.1 -successor-port=10000
+./node -ip=127.0.0.1 -port=10002 -successor-ip=127.0.0.1 -successor-port=10000
+
+#insert data on dht
+curl -X POST http://127.0.0.1:10000/key/hello -d "data=hello"
+
+#check all three node can access to data
+curl -X GET http://127.0.0.1:10000/key/hello
+curl -X GET http://127.0.0.1:10001/key/hello
+curl -X GET http://127.0.0.1:10002/key/hello
